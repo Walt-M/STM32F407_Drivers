085ef28532528e40f9423007cb9e4ed90a8655f9
Walt-M <waltermanter62@gmail.com>
2023-02-10 12:46:40 -0500

    Defined macros for SPI CR1 and status regs.

--- Inc/spi.h
+++ Inc/spi.h
@@ -17,22 +17,17 @@
 
 typedef struct
 {
-    uint32_t Mode;                      /*Two line mode has uni-directional pins,
-                                          One line mode has bi-directional pins.*/
+    uint32_t Mode;                      /*Selects device as master or slave*/
                                           
-    uint32_t CRCEnable;                 /*Enable CRC Calculation. Todo: Figure out what this register does.*/ 
+    uint32_t CommunicationType;                 /*Enable CRC Calculation. Todo: Figure out what this register does.*/ 
 
     uint32_t DataFrame;                 /*Select the transmit and receive data size. 8 bits or 16.*/
 
-    uint32_t SoftwareSlaveManagement;   /*Controls software slave management*/
+    uint32_t SSM;   /*Controls software slave management*/
 
     uint32_t SigBitFrame;               /*Chooses if the LSB or the MSB is sent first*/
 
-    uint32_t SPIEnable;                 /*Enable or disable SPI peripheral.*/
-
     uint32_t BaudRate;                  /*Changes the clock divisor. lowest divisor is 2*/
-
-    uint32_t MasterSelect;              /*Configure device as a master or slave*/
 
     uint32_t ClockPolarity;             /*Set clock as defualt high or low when clock is off*/
 
@@ -40,49 +35,90 @@ typedef struct
 
 }SPI_Config;
 
-#define SPI_Mode_2Line = (0U)
-#define SPI_Mode_2Line_RxOnly = (1 << 10)
-#define SPI_Mode_1Line_Rx = (1 << 15)
-#define SPI_Mode_1Line_Tx = (3 << 14)
 
-#define SPI_CRC_Di = 0U
-#define SPI_CRC_En = (1 << 13)
+/*Defines various modes that the SPI communication protocol
+can be set to*/
 
-#define SPI_CRCNext_Data = (0U)
-#define SPI_CRCNext_CRC = (1 << 12)
+#define SPI_DISABLE (0U)
+#define SPI_ENABLE (1 << 6)
 
-#define SPI_DataFrame_8bit = (0U)
-#define SPI_DataFrame_16bit = (1 << 11)
+#define SPI_MODE (1 << 2)
+#define SPI_MODE_SLAVE (0U)
+#define SPI_MODE_MASTER SPI_MODE 
 
-#define SPI_SSM_Di = (0U)
-#define SPI_SSM_En = (3 << 9)
+#define SPI_COMMUNICATIONTYPE_FULLDUPLEX (0U)
+#define SPI_COMMUNICATIONTYPE_HALFDUPLEX_RX (1 << 15)
+#define SPI_COMMUNICATIONTYPE_SIMPLEX (1 << 10)
 
-#define SPI_SigFigFrame_MSB = (0U)
-#define SPI_SigFigFrame_LSB = (1 << 7)
+#define SPI_DATAFRAME (1 << 11)
+#define SPI_DATAFRAME_8BIT (0U)
+#define SPI_DATAFRAME_16BIT SPI_DATAFRAME
 
-#define SPI_Di = (0U)
-#define SPI_En = (1 << 6)
+#define SPI_SSM_DISABLE (0U)
+#define SPI_SSM_SOFT_ENABLE (1 << 9)
+#define SPI_SSM_ENABLE (3 << 8)
 
-#define SPI_BaudRate_2 = (0U)
-#define SPI_BaudRate_4 = (1 << 3)
-#define SPI_BaudRate_8 = (2 << 3)
-#define SPI_BaudRate_16 = (3 << 3)
-#define SPI_BaudRate_32 = (4 << 3)
-#define SPI_BaudRate_64 = (5 << 3)
-#define SPI_BaudRate_128 = (6 << 3)
-#define SPI_BaudRate_256 = (7 << 3)
+#define SPI_SIGFIGFRAME_MSB (0U)
+#define SPI_SIGFIGFRAME_LSB (1 << 7)
 
-#define SPI_MasterSelect_Di = (0U)
-#define SPI_MasterSelect_En = (1 << 2)
+#define SPI_BAUDRATE_2 (0U)
+#define SPI_BAUDRATE_4 (1 << 3)
+#define SPI_BAUDRATE_8 (2 << 3)
+#define SPI_BAUDRATE_16 (3 << 3)
+#define SPI_BAUDRATE_32 (4 << 3)
+#define SPI_BAUDRATE_64 (5 << 3)
+#define SPI_BAUDRATE_128 (6 << 3)
+#define SPI_BAUDRATE_256 (7 << 3)
 
-#define SPI_ClockPolarity_Lo = (0U)
-#define SPI_ClockPolarity_Hi = (1 << 1)
+#define SPI_CLOCKPOLARITY_Lo (0U)
+#define SPI_CLOCKPOLARITY_Hi (1 << 1)
 
-#define SPI_ClockPhase_1Edge = (0U)
-#define SPI_ClockPhase_2Edge = (1)
+#define SPI_CLOCKPHASE_EDGE1 (0U)
+#define SPI_CLOCKPHASE_EDGE2 (1)
+
+
+/*Defines statuses for the various states that the SPI
+Communication line can have.*/
+
+#define SPI_STATUS_RX (1 << 0)
+#define SPI_STATUS_RX_EMPTY (0U)
+#define SPI_STATUS_RX_NOTEMPTY SPI_STATUS_RX
+
+#define SPI_STATUS_TX (1 << 1)
+#define SPI_STATUS_TX_EMPTY (0U)
+#define SPI_STATUS_TX_NOTEMPTY SPI_STATUS_TX
+
+#define SPI_STATUS_CHANNEL (1 << 2)
+#define SPI_STATUS_CHANNEL_LEFT (0U)
+#define SPI_STATUS_CHANNEL_RIGHT SPI_STATUS_CHANNEL
+
+#define SPI_STATUS_UNDERRUN (1 << 3)
+#define SPI_STATUS_UNDERRUN_NONE (0U)
+#define SPI_STATUS_UNDERRUN_OCCURRED SPI_STATUS_UNDERRUN
+
+#define SPI_STATUS_CRC_ERROR (1 << 4)
+#define SPI_STATUS_CRC_ERROR_NONE (0U)
+#define SPI_STATUS_CRC_ERROR_OCCURRED SPI_STATUS_CRC_ERROR
+
+#define SPI_STATUS_MODE_FAULT (1 << 5)
+#define SPI_STATUS_MODE_FAULT_NONE (0U)
+#define SPI_STATUS_MODE_FAULT_OCCURRED SPI_STATUS_MODE_FAULT
+
+#define SPI_STATUS_OVERRUN (1 << 6)
+#define SPI_STATUS_OVERRUN_NONE (0U)
+#define SPI_STATUS_OVERRUN_OCCURRED SPI_STATUS_OVERRUN
+
+#define SPI_STATUS_NOT_BUSY (0U)
+#define SPI_STATUS_BUSY (1 << 7)
+
+#define SPI_STATUS_FRAME_ERROR (1 << 8)
+#define SPI_STATUS_FRAME_ERROR_NONE (0U)
+#define SPI_STATUS_FRAME_ERROR_OCCURRED SPI_STATUS_FRAME_ERROR
+
+
 
 void SPI_Init(SPI_TypeDef *SPIx, SPI_Config *SPI_Config);
 void SPI_DeInit(SPI_TypeDef *SPIx);
 
-void SPI_Transmit(SPI_TypeDef *SPIx, uint8_t *Data, uint32_t Size);
+void SPI_Transmit(SPI_TypeDef *SPIx, uint16_t *Data, uint32_t Size);
 void SPI_Receive(SPI_TypeDef *SPIx, uint8_t *Data, uint32_t Size);
--- Src/spi.c
+++ Src/spi.c
@@ -1,25 +1,38 @@
 #include "spi.h"
 
 void SPI_Init(SPI_TypeDef *SPIx, SPI_Config *SPI_Config){
+    SPIx->CR1 &= (0x0000);
+
+    SPIx->CR1 |= SPI_ENABLE;
     SPIx->CR1 |= SPI_Config->Mode;
-    SPIx->CR1 |= SPI_Config->CRCEnable;
+    SPIx->CR1 |= SPI_Config->CommunicationType;
     SPIx->CR1 |= SPI_Config->DataFrame;
-    SPIx->CR1 |= SPI_Config->SoftwareSlaveManagement;
+    SPIx->CR1 |= SPI_Config->SSM;
     SPIx->CR1 |= SPI_Config->SigBitFrame;
-    SPIx->CR1 |= SPI_Config->SPIEnable;
     SPIx->CR1 |= SPI_Config->BaudRate;
-    SPIx->CR1 |= SPI_Config->MasterSelect;
     SPIx->CR1 |= SPI_Config->ClockPolarity;
     SPIx->CR1 |= SPI_Config->ClockPhase;
 }
 
 void SPI_DeInit(SPI_TypeDef *SPIx){
-    SPIx->CR1 &= (0U);
+    SPIx->CR1 &= (0x0000);
 }
 
-void SPI_Transmit(SPI_TypeDef *SPIx, uint8_t *Data, uint32_t Size){
-
+void SPI_Transmit(SPI_TypeDef *SPIx, uint16_t *Data, uint32_t Size){
+    while (Size != 0)
+    { 
+        if (SPIx->CR1 & SPI_DATAFRAME == SPI_DATAFRAME_16BIT)
+        {
+            /*TODO: Write code here if it is 16 bits*/
+        }
+        else
+        {
+            /*TODO: Write code here if it is 8 bits.*/
+        }       
+    }
 }
+
+
 
 void SPI_Receive(SPI_TypeDef *SPIx, uint8_t *Data, uint32_t Size){
 
